657c3840243e7af2249b412d12e4c3d48c8d761c
Paul Tagliamonte <tag@pault.ag>
2014-06-14 00:06:17 -0400

    fiddle.

--- loki/server.py
+++ loki/server.py
@@ -3,7 +3,13 @@ import asyncio
 import aiohttp
 import aiohttp.server
 
-API = "http://api.opencivicdata.org"
+
+def create_proxy(api):
+    def _():
+        p = HttpProxyHandler()
+        p.api = api
+        return p
+    return _
 
 
 class HttpProxyHandler(aiohttp.server.ServerHttpProtocol):
@@ -12,35 +18,20 @@ class HttpProxyHandler(aiohttp.server.ServerHttpProtocol):
         path = message.path
         method = message.method
 
-        remote = yield from aiohttp.request(method, "%s/%s" % (API, path))
+        remote = yield from aiohttp.request(method, "%s/%s" % (self.api, path))
         headers = remote.headers.items()
         headers = list(headers) + [("X-Loki-Version", "nil")]
-
         resp = aiohttp.Response(
             self.writer,
             remote.status,
             http_version=message.version
         )
-
         resp.add_headers(*headers)
         resp.send_headers()
-
-        ## XXX: Insert manglers here.
-
-        chunk = yield from remote.content.read()
-        resp.write(chunk)
+        while True:
+            try:
+                chunk = yield from remote.content.read()
+                resp.write(chunk)
+            except aiohttp.parsers.EofStream:
+                break
         resp.write_eof()
-
-
-loop = asyncio.get_event_loop()
-coro = loop.create_server(HttpProxyHandler, '127.0.0.1', 8888)
-server = loop.run_until_complete(coro)
-print('serving on {}'.format(server.sockets[0].getsockname()))
-
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    print("exit")
-finally:
-    server.close()
-    loop.close()
